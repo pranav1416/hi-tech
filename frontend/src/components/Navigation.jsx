f8ffad723b5605177a5ce99b8177002326b881db
Nour Alquraini <nouralquraini@csu.fullerton.edu>
2021-04-21 18:50:33 -0700

    new changes

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -5,19 +5,32 @@ import { LinkContainer } from 'react-router-bootstrap'
 import { Row, Col, Tab} from 'react-bootstrap'
 import { Fragment, Component, render} from "react"
 import ListGroup from 'react-bootstrap/ListGroup'
+import { get } from 'mongoose';
+
+// function handleClick(Tab) {   
+//   Tab.preventDefault();  
+//     console.log('The tab was clicked.')
+//      }
+
+// const [NavigationState, setNavigationState] = useState("/browser")
+
 
 
 const Navigation = () => {
-  return(
-  
+  return(  
     <Tab.Container id="left-tabs-example" defaultActiveKey="first">
   <Row style={{ width: '45%'}}>
     <Col sm={3}>
-      <Nav variant="pills" className="flex-column">
+    <Nav variant="pills" className="flex-column">
+      {/* value={NavigationState}
+       onChange = {(e) => {
+        const selectedNavigation = e.target.value
+        setNavigationState(selectedNavigation)
+     }
+      }> */}
         <Nav.Item>
-          <Nav.Link eventKey="first">All Categories</Nav.Link>
+          <Nav.Link /*onClick={handleClick}*/ href= '/browser' eventKey="first">All Categories</Nav.Link>
         </Nav.Item>
-
         <Nav.Item>
           <Nav.Link eventKey="second">Laptops</Nav.Link>
         </Nav.Item>
@@ -41,4 +54,6 @@ const Navigation = () => {
 
    )
   }
+  
  export default Navigation
+
