b6f054165729788a83decd3f1f28962699744684
Pranav Borole <axel@Pranavs-MacBook-Pro.local>
2021-04-27 18:41:18 -0700

    Debug product image

--- frontend/src/components/ProductImage.jsx
+++ frontend/src/components/ProductImage.jsx
@@ -4,7 +4,7 @@ import { Figure } from 'react-bootstrap'
 const ProductImage = ({ product }) => {
   return (
     <Figure>
-      <Figure.Image width={400} height={420} src={product.imageURLs[0]} />
+      <Figure.Image width={400} height={420} src={product.image} />
       <Figure.Caption>{product.description}</Figure.Caption>
     </Figure>
   )
