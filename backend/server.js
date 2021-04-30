e4fb5b9b96bae8e06cbe43807a1645ca977d2af7
Shraddha Yadav <shraddha.yadav176@gmail.com>
2021-04-29 19:15:16 -0700

    Fix1

--- backend/server.js
+++ backend/server.js
@@ -48,11 +48,13 @@ app.use(errorHandler)
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../frontend/build')));
-}
-app.get('*',(req,res)=>{
+
+  app.get('*',(req,res)=>{
     // res.sendFile(path.join(__dirname,'/../frontend','build','index.html'));
     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-});
+  });
+}
+
 
 const PORT = process.env.PORT || 5001
 app.listen(
