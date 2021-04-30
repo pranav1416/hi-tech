a41398f578778ef784cc0e9f794784dc59ed7df2
Shraddha Yadav <shraddha.yadav176@gmail.com>
2021-04-29 17:40:07 -0700

    Fix package.json

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static('frontend/build'));
 }
 app.get('*',(req,res)=>{
-res.sendFile(path.join(__dirname,'frontend','build','index.html'));
+res.sendFile(path.join(__dirname,'/../frontend','build','index.html'));
 });
 
 const PORT = process.env.PORT || 5001
