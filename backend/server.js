420b2b2aae5fa81cdc0c1997b8998b2e3384b649
Shraddha Yadav <shraddha.yadav176@gmail.com>
2021-04-29 17:47:10 -0700

    fix express.static path

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,10 @@ app.use(notFound)
 app.use(errorHandler)
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('frontend/build'));
+  app.use(express.static('../frontend/build'));
 }
 app.get('*',(req,res)=>{
-res.sendFile(path.join(__dirname,'/../frontend','build','index.html'));
+    res.sendFile(path.join(__dirname,'/../frontend','build','index.html'));
 });
 
 const PORT = process.env.PORT || 5001
