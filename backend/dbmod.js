314bb10e2c78478ab9a3c0d3fcd8d76f43c99ff7
Pranav Borole <axel@Pranavs-MacBook-Pro.local>
2021-05-08 02:39:43 -0700

    final build

--- backend/dbmod.js
+++ backend/dbmod.js
@@ -1,63 +1,63 @@
-import products from './allprod.js'
-import { createRequire } from 'module'
-import fs from 'fs'
+// import products from './allprod.js'
+// import { createRequire } from 'module'
+// import fs from 'fs'
 
-const require = createRequire(import.meta.url)
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+// const require = createRequire(import.meta.url)
+// const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 
-const res = []
+// const res = []
 
-const tempFunction = (data) => {
-  //console.log(data)
-  fs.writeFile('dataFinal.json', JSON.stringify(data), (err) => {
-    if (err) {
-      console.error(err)
-    }
-    console.log('File has been created')
-  })
-}
+// const tempFunction = (data) => {
+//   //console.log(data)
+//   fs.writeFile('dataFinal.json', JSON.stringify(data), (err) => {
+//     if (err) {
+//       console.error(err)
+//     }
+//     console.log('File has been created')
+//   })
+// }
 
-const findInvalidUrl = (products) => {
-  var count = 0
-  for (var i = 500; i < products.length; i++) {
-    var doc = products[i]
-    const beforeLen = doc.imageURLs.length
+// const findInvalidUrl = (products) => {
+//   var count = 0
+//   for (var i = 500; i < products.length; i++) {
+//     var doc = products[i]
+//     const beforeLen = doc.imageURLs.length
 
-    doc.imageURLs.forEach((imgUrl) => {
-      let http = new XMLHttpRequest()
-      http.open('GET', imgUrl, true)
-      http.send()
-      if (http.status !== 200) {
-        doc.imageURLs.pop(imgUrl)
-        http.abort()
-      }
-      http.abort()
-    })
-    const afterLen = doc.imageURLs.length
-    if (afterLen > 10) {
-      const newImageURLs = doc.imageURLs.slice(0, 10)
-      doc.imageURLs = newImageURLs
-    }
-    const afterAfterLen = doc.imageURLs.length
-    count += 1
-    res.push(doc)
-    console.log(count, ' : ', beforeLen, ' , ', afterLen, ' , ', afterAfterLen)
-  }
-  // .forEach((doc) => {
-  //   const beforeLen = doc.imageURLs.length
-  //   doc.imageURLs.forEach((imgUrl) => {
-  //     let http = new XMLHttpRequest()
-  //     http.open('GET', imgUrl, false)
-  //     http.send()
-  //     if (http.status !== 200) {
-  //       doc.imageURLs.pop(imgUrl)
-  //     }
-  //   })
-  //   const afterLen = doc.imageURLs.length
-  //   count += 1
-  //   console.log(count, ' : ', beforeLen, ' , ', afterLen)
-  // })
-  tempFunction(res)
-}
+//     doc.imageURLs.forEach((imgUrl) => {
+//       let http = new XMLHttpRequest()
+//       http.open('GET', imgUrl, true)
+//       http.send()
+//       if (http.status !== 200) {
+//         doc.imageURLs.pop(imgUrl)
+//         http.abort()
+//       }
+//       http.abort()
+//     })
+//     const afterLen = doc.imageURLs.length
+//     if (afterLen > 10) {
+//       const newImageURLs = doc.imageURLs.slice(0, 10)
+//       doc.imageURLs = newImageURLs
+//     }
+//     const afterAfterLen = doc.imageURLs.length
+//     count += 1
+//     res.push(doc)
+//     console.log(count, ' : ', beforeLen, ' , ', afterLen, ' , ', afterAfterLen)
+//   }
+//   // .forEach((doc) => {
+//   //   const beforeLen = doc.imageURLs.length
+//   //   doc.imageURLs.forEach((imgUrl) => {
+//   //     let http = new XMLHttpRequest()
+//   //     http.open('GET', imgUrl, false)
+//   //     http.send()
+//   //     if (http.status !== 200) {
+//   //       doc.imageURLs.pop(imgUrl)
+//   //     }
+//   //   })
+//   //   const afterLen = doc.imageURLs.length
+//   //   count += 1
+//   //   console.log(count, ' : ', beforeLen, ' , ', afterLen)
+//   // })
+//   tempFunction(res)
+// }
 
-findInvalidUrl(products)
+// findInvalidUrl(products)
--- backend/seed.js
+++ backend/seed.js
@@ -1,102 +1,102 @@
-db = db.getSiblingDB('hitech')
+// db = db.getSiblingDB('hitech')
 
-print(db.getCollectionNames())
+// print(db.getCollectionNames())
 
-var products = db.products
-// var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
-// For checking broken images
-products.find({}).forEach((doc) => {
-  //var images = doc.imageURls
-  var invalidUrls = []
-  doc.imageURLs.forEach((imgUrl) => {
-    var request = new XMLHttpRequest()
-    request.open('GET', imgUrl, true)
-    request.send()
-    request.onload = function () {
-      status = request.status
-      if (request.status !== 200) {
-        invalidUrls.push(imgUrl)
-      }
-    }
-    print(invalidUrls)
-  })
-})
-
-// var categ = [
-//   'Audio',
-//   'Computers',
-//   'TV',
-//   'Mobile',
-//   'Cameras & Camcorders',
-//   'Car Electronics',
-// ]
-// var x = 0
-// For updating primary Categories of database
+// var products = db.products
+// // var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+// // For checking broken images
 // products.find({}).forEach((doc) => {
-//   var catg = doc.categories.reverse()
-//   var flag = categ.forEach((cat) => {
-//     if (catg.includes(cat)) {
-//       db.products.findAndModify({
-//         query: { _id: doc._id },
-//         update: { $set: { primaryCategories: cat } },
-//         upsert: false,
-//       })
-//       return true
+//   //var images = doc.imageURls
+//   var invalidUrls = []
+//   doc.imageURLs.forEach((imgUrl) => {
+//     var request = new XMLHttpRequest()
+//     request.open('GET', imgUrl, true)
+//     request.send()
+//     request.onload = function () {
+//       status = request.status
+//       if (request.status !== 200) {
+//         invalidUrls.push(imgUrl)
+//       }
 //     }
-//     print(x + 1, ' : ', flag, '\n')
+//     print(invalidUrls)
 //   })
 // })
 
-// db.products.findAndModify({
-//   query: { upc: ids },
-//   update: { $set: { categories: dats, imageURLs: imgs } },
-//   upsert: false,
-// })
+// // var categ = [
+// //   'Audio',
+// //   'Computers',
+// //   'TV',
+// //   'Mobile',
+// //   'Cameras & Camcorders',
+// //   'Car Electronics',
+// // ]
+// // var x = 0
+// // For updating primary Categories of database
+// // products.find({}).forEach((doc) => {
+// //   var catg = doc.categories.reverse()
+// //   var flag = categ.forEach((cat) => {
+// //     if (catg.includes(cat)) {
+// //       db.products.findAndModify({
+// //         query: { _id: doc._id },
+// //         update: { $set: { primaryCategories: cat } },
+// //         upsert: false,
+// //       })
+// //       return true
+// //     }
+// //     print(x + 1, ' : ', flag, '\n')
+// //   })
+// // })
 
-// Find and modify categories and imageURLs from string to array
+// // db.products.findAndModify({
+// //   query: { upc: ids },
+// //   update: { $set: { categories: dats, imageURLs: imgs } },
+// //   upsert: false,
+// // })
 
-// prod.find({}).forEach((doc) => {
-//   var catg = doc.categories.split(',')
-//   var imgs = doc.imageURLs.split(',')
-//   var ids = doc.upc
-//   db.products.findAndModify({
-//     query: { upc: ids },
-//     update: { $set: { categories: catg, imageURLs: imgs } },
-//     upsert: false,
-//   })
-// })
+// // Find and modify categories and imageURLs from string to array
 
-// db.products.updateOne({ upc: '50616008921'}, {
-//     $set: { }
-// })
+// // prod.find({}).forEach((doc) => {
+// //   var catg = doc.categories.split(',')
+// //   var imgs = doc.imageURLs.split(',')
+// //   var ids = doc.upc
+// //   db.products.findAndModify({
+// //     query: { upc: ids },
+// //     update: { $set: { categories: catg, imageURLs: imgs } },
+// //     upsert: false,
+// //   })
+// // })
 
-// Removing duplicates based on UPC
+// // db.products.updateOne({ upc: '50616008921'}, {
+// //     $set: { }
+// // })
 
-// db.products
-//   .aggregate([
-//     {
-//       $group: {
-//         _id: { upc: '$upc' },
-//         dups: { $addToSet: '$_id' },
-//         count: { $sum: 1 },
-//       },
-//     },
-//     {
-//       $match: {
-//         count: { $gt: 1 },
-//       },
-//     },
-//   ])
-//   .forEach(function (doc) {
-//     doc.dups.shift()
-//     db.products.remove({
-//       _id: { $in: doc.dups },
-//     })
-//   })
+// // Removing duplicates based on UPC
 
-// // Random values for countInStock
+// // db.products
+// //   .aggregate([
+// //     {
+// //       $group: {
+// //         _id: { upc: '$upc' },
+// //         dups: { $addToSet: '$_id' },
+// //         count: { $sum: 1 },
+// //       },
+// //     },
+// //     {
+// //       $match: {
+// //         count: { $gt: 1 },
+// //       },
+// //     },
+// //   ])
+// //   .forEach(function (doc) {
+// //     doc.dups.shift()
+// //     db.products.remove({
+// //       _id: { $in: doc.dups },
+// //     })
+// //   })
 
-// db.products.aggregate([
-//   { $set: { countInStock: { $multiply: [{ $rand: {} }, 10] } } },
-//   { $set: { countInStock: { $floor: '$countInStock' } } },
-// ])
+// // // Random values for countInStock
+
+// // db.products.aggregate([
+// //   { $set: { countInStock: { $multiply: [{ $rand: {} }, 10] } } },
+// //   { $set: { countInStock: { $floor: '$countInStock' } } },
+// // ])
